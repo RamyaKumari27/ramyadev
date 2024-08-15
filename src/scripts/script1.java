56b7df1293de8b00a3011d2599fa0a5504cb9401
Ramya Gowda <Ramya Gowda@LAPTOP-OL29EVL3>
2024-08-15 15:04:24 +0530

    hi commit

---
+++ src/scripts/script1.java
@@ -0,0 +1,28 @@
+package scripts;
+
+public class script1 {
+
+	public static void main(String[] args) {
+
+			for(int i=1;i<=3;i++)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+
+			for(int i=4;i>=0;i--)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					System.out.print("*");
+				}
+				System.out.println();
+
+		}
+		}
+	
+
+}
